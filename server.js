3d119c17a08c5f3aad2b170e26efe6f0d26bd513
Punkinut <heypunkinut@gmail.com>
2021-06-01 10:26:06 +1000

    Setup basic paramter for homeroutes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
 const path = require('path');
-const sequelize = require('./config/connection');
+const express = require('express');
 const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;

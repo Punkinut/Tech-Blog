ddf6817e6d92323de6bf11ae9258482f06d43888
Punkinut <heypunkinut@gmail.com>
2021-06-02 11:41:52 +1000

    Changed seeding information

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
     format_time: (date) => {
-      const day = date.getDay();
+      const day = date.getDay() - 1;
       const month = date.toLocaleString('en-us', { month: 'short' });;
       const year = date.getFullYear();
       return `${day} ${month} ${year}`;

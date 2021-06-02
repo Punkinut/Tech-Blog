511eef35de97f47bd930445ea04c285332edefcd
Punkinut <heypunkinut@gmail.com>
2021-06-02 11:26:23 +1000

    Blog now shows posts from database

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
+const { User, Post} = require('../models');
 
 router.get('/', async (req, res) => {
     try {
-        res.render('home');
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true}));
+        res.render('home', {
+            posts
+        });
     } catch (err) {
         res.status(500).json(err);
     }

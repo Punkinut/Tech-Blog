98817d9e113cc9ed81c6246f4f97bc5c805b35fc
Punkinut <heypunkinut@gmail.com>
2021-06-03 10:23:03 +1000

    Added front end login javascript

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,22 @@
-console.log('Works')
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = $('#email-login').val().trim();
+    const password = $('#password-login').val().trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+          });
+
+          if (response.ok) {
+            console.log('Works')
+          } else {
+            alert(response.statusText);
+          }
+    }
+};
+
+$('.login-form').on('submit', loginFormHandler);

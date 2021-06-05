2bc47ba8d19327f7cef4800f51bc45717a8c2bbb
Punkinut <heypunkinut@gmail.com>
2021-06-05 14:21:40 +1000

    Redesigned navigation for mobile

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,8 @@ const pathname = document.location.pathname
 if ( pathname === '/redirect') {
     $('.navigation').css('display', 'none')
 }
+
+if ( pathname === '/dashboard') {
+    $('.dash').css('display', 'none')
+    $('.home').css('display', 'block')
+}

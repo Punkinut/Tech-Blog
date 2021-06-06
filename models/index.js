47c757510f27d4961fa07762d096322daeedd3a1
Punkinut <heypunkinut@gmail.com>
2021-06-06 10:29:13 +1000

    Added comment model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment')
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
@@ -7,6 +8,24 @@ User.hasMany(Post, {
 });
 
 Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
   foreignKey: 'user_id'
 });
 

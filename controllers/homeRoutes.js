a6b66651e7a9ceac63bef1e80bd5ff6baab640b0
Punkinut <heypunkinut@gmail.com>
2021-06-19 10:27:53 +1000

    Added change icon get route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -81,6 +81,17 @@ router.get('/dashboard', redirect, async (req, res) => {
     }
 })
 
+router.get('/changeicon', redirect, async (req, res) => {
+    try {
+        res.render('changeIcon', {
+            logged_in: req.session.logged_in,
+            user_id: req.session.user_id
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 router.get('/redirect', withAuth, async (req, res) => {
     try {
         res.render('redirect');

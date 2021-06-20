03d2ede1895e980a6c8838b8412ca1a137051cb8
Punkinut <heypunkinut@gmail.com>
2021-06-20 14:58:02 +1000

    Added model for hearts

--- models/index.js
+++ models/index.js
@@ -1,6 +1,12 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comment = require('./Comment')
+const Comment = require('./Comment');
+const hasLiked = require('./hasLiked');
+
+User.hasMany(hasLiked, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+})
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
@@ -29,4 +35,4 @@ Comment.belongsTo(Post, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post, Comment };
+module.exports = { User, Post, Comment, hasLiked };
